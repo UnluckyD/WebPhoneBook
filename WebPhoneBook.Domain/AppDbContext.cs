63eb9c530a05c85f6e8966bb58dbf2de96a36784
LuckMuff <ammobarbital@gmail.com>
2021-12-03 16:06:54 +0300

    Try connect db from ConfigureServices

---
+++ WebPhoneBook.Domain/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPhoneBook.Domain
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<PhoneBook> PhoneBooks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<PhoneBook>().HasData( new PhoneBook
+            {
+                Id = 1,
+                LastName = "lastName",
+                FirstName = "firstName",
+                MiddleName = "MiddleName"
+            } );
+        }
+    }
+}
--- WebPhoneBook/Startup.cs
+++ WebPhoneBook/Startup.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WebPhoneBook.DataBaseAccess;
+using System.Text;
+using WebPhoneBook.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebPhoneBook
 {
@@ -15,17 +16,23 @@ namespace WebPhoneBook
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+            "Data Source=(local); Database=Articles; Persist Security Info=False; MultipleActiveResultSets=True; Trusted_Connection=True;"
+            ));
         }
 
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            DataBase PhoneBook = new DataBase();
-
+            List<PhoneBook> list = new List<PhoneBook>() { new PhoneBook { Id = 1, LastName = "lastName", FirstName = "firstName", MiddleName = "MiddleName" },
+            new PhoneBook { Id = 2, LastName = "lastName", FirstName = "firstName", MiddleName = "MiddleName" },
+            new PhoneBook { Id = 3, LastName = "lastName", FirstName = "firstName", MiddleName = "MiddleName" },
+            new PhoneBook { Id = 4, LastName = "lastName", FirstName = "firstName", MiddleName = "MiddleName" },
+            new PhoneBook { Id = 5, LastName = "lastName", FirstName = "firstName", MiddleName = "MiddleName" }};
 
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
@@ -34,7 +41,23 @@ namespace WebPhoneBook
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync($"Hello World! DataBase is: {PhoneBook != null}");
+                    var content = new StringBuilder();
+                    content.Append("<table border='2'>");
+
+                    foreach (var person in list)
+                    {
+                        content.Append("<tr>");
+                        content.Append($"<td><font color='black' size='5'>{person.Id}</font></td>");
+                        content.Append($"<td><font color='black' size='5'>{person.LastName}</font></td>");
+                        content.Append($"<td><font color='black' size='5'>{person.FirstName}</font></td>");
+                        content.Append($"<td><font color='black' size='5'>{person.MiddleName}</font></td>");
+                        content.Append("</tr>");
+                    }
+
+
+                    content.Append("</table");
+
+                    await context.Response.WriteAsync($"<!DOCTYPE html>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n{content}");
                 });
             });
         }
